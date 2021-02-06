b139945fc3ff46cfccde05ef1f349f7e475a97d8
Ulises de Jesus Hernandez Hernandez <70914091+UlisesHdzHdz@users.noreply.github.com>
2021-02-06 12:21:36 -0600

    first commit config web pack

--- app/Login.js
+++ app/Login.js
@@ -4,7 +4,7 @@ class Login extends React.Component{
     render() {
 
         return (
-            <h2>get.frefree.deimons undate chinoantrax = 1000 deimins pull full id = 1 cerca de 100 metros  </h2>
+            <h2>hola mundo</h2>
         )
     }
 }
